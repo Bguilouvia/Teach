e31c0efb4b5977bb94f4b922c835a0c600f9058e
Ghislain Nono Gueye <nonoghislain@gmail.com>
2018-08-08 21:26:47 -0500

    Improved assign_grade with vectorized features.

--- assign_grade.R
+++ assign_grade.R
@@ -1,6 +1,7 @@
-assign_grade<-function(score){
-  if (score<90) grade<-"A"
-  if (score<90 & score >=80) grade <-"B"
-  else grade <- "F"
-  return(grade)
+assign_grade <- function(score){
+  cut(score, 
+      breaks = c(-Inf, 60, 70, 80, 90, Inf), 
+      labels = c("F", "D", "C", "B", "A"),
+      include.lowest = TRUE,
+      right = FALSE)
 }
